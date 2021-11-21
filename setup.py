f1e61ad655510ead070e032124970e3756e25a7e
Franziska Kunsmann <hi@kunsmann.eu>
2021-11-21 13:35:20 +0100

    release 0.1.2

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ cmdclass = {"build": CustomBuild}
 
 setup(
     name="pretalx-plugin-lower-thirds",
-    version="0.1.1",
+    version="0.1.2",
     description=(
         "Creates lower thirds from your current schedule. Will show "
         "speaker names and talk title using the configured track and "
