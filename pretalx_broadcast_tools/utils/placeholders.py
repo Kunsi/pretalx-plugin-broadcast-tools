11afb886023aa1113f2bd27c466ed567a88cd320
Franziska Kunsmann <hi@kunsmann.eu>
2023-02-28 10:17:50 +0100

    add TALK_URL and FEEDBACK_URL to the list of placeholders

--- pretalx_broadcast_tools/utils/placeholders.py
+++ pretalx_broadcast_tools/utils/placeholders.py
@@ -1,6 +1,16 @@
+from django.conf import settings
+
 def placeholders(schedule, talk):
     return {
-        "EVENT_SLUG": str(schedule.event.slug),
-        "TALK_SLUG": talk.frab_slug,
         "CODE": talk.submission.code,
+        "EVENT_SLUG": str(schedule.event.slug),
+        "FEEDBACK_URL": "{}{}".format(
+            schedule.event.custom_domain or settings.SITE_URL,
+            talk.submission.urls.feedback,
+        ),
+        "TALK_SLUG": talk.frab_slug,
+        "TALK_URL": "{}{}".format(
+            schedule.event.custom_domain or settings.SITE_URL,
+            talk.submission.urls.public,
+        ),
     }
